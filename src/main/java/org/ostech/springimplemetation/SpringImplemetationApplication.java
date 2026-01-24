c7883da9e36bccd017a27939c804f0204c1a5238
amilasilva7 <osapsilva@gmail.com>
2026-01-24 06:53:26 +0530

    first commit

---
+++ src/main/java/org/ostech/springimplemetation/SpringImplemetationApplication.java
@@ -0,0 +1,13 @@
+package org.ostech.springimplemetation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringImplemetationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringImplemetationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ostech/springimplemetation/SpringImplemetationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ostech.springimplemetation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringImplemetationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
